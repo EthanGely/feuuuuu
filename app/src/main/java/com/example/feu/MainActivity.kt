55eed2599b5d0b8f1f5fcd73026b44406e8f622e
Ethan <ethan.gely@my-digital-school.org>
2025-03-05 13:58:33 +0100

    fix bordersd

--- app/src/main/java/com/example/feu/MainActivity.kt
+++ app/src/main/java/com/example/feu/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.feu
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Ensure content does not overlap with cutouts (notch, camera)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
